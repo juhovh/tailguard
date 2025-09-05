5acd8a0085477faf4200fb97fb8e54b40b4701ca
Juho Vähä-Herttua <juhovh@iki.fi>
2025-09-06 00:09:43 +1000

    Add more checks for env variables, set up firewall two ways

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 set -e
 
+# Check that the WireGuard device env variable is set
+if [ -z "${WG_DEVICE+set}" ]; then
+  echo "Environment variable \$WG_DEVICE is not set, defaulting to wg0"
+  export WG_DEVICE="wg0"
+fi
+
+# Check that the config file exists and has the right permissions
+WG_CONF_PATH="/etc/wireguard/${WG_DEVICE}.conf"
+if [ ! -f "${WG_CONF_PATH}" ]; then
+  echo "Config file ${WG_CONF_PATH} does not exist, exiting"
+  exit 1
+fi
+chmod 600 "${WG_CONF_PATH}"
+
+# Check that the Tailscale device env variable is set
+if [ -z "${TS_DEVICE+set}" ]; then
+  echo "Environment variable \$TS_DEVICE is not set, defaulting to tailscale0"
+  export TS_DEVICE="tailscale0"
+fi
+
+# Check that the Tailscale port env variable is set
+if [ -z "${TS_PORT+set}" ]; then
+  echo "Environment variable \$TS_PORT is not set, defaulting to 41641"
+  export TS_PORT="41641"
+fi
+
 if [ -n "${TS_AUTHKEY}" ]; then
   if case "${TS_AUTHKEY}" in "tskey-auth-"*) true ;; *) false ;; esac; then
     echo "Found a valid tailscale auth key, using it to perform authentication"
@@ -10,20 +36,6 @@ if [ -n "${TS_AUTHKEY}" ]; then
     export -n TS_AUTHKEY
   fi
 fi
-
-# Check that the device env variable is set
-if [ -z "${WG_DEVICE+set}" ]; then
-  echo "Environment variable \$WG_DEVICE is not set"
-  exit 1
-fi
-
-# Check that the config file exists and has the right permissions
-WG_CONF_PATH="/etc/wireguard/${WG_DEVICE}.conf"
-if [ ! -f "${WG_CONF_PATH}" ]; then
-  echo "Config file ${WG_CONF_PATH} does not exist"
-  exit 1
-fi
-chmod 600 "${WG_CONF_PATH}"
 
 # Create wireguard device and set it up
 echo "******************************"
@@ -43,6 +55,12 @@ export TS_AUTH_ONCE="true"
 export TS_USERSPACE="false"
 export TS_STATE_DIR="/tailguard/state"
 export TS_KUBE_SECRET=""
-export TS_TAILSCALED_EXTRA_ARGS="--port=${TS_PORT}"
+export TS_TAILSCALED_EXTRA_ARGS="--tun="${TS_DEVICE}" --port=${TS_PORT}"
+
+# Create firewall chains for enforcing tunneling between devices
+iptables -N tg-forward
+iptables -A tg-forward -s 100.64.0.0/10 ! -o "${WG_DEVICE}" -j DROP
+ip6tables -N tg-forward
+ip6tables -A tg-forward -s fd7a:115c:a1e0::/48 ! -o "${WG_DEVICE}" -j DROP
 
 /usr/local/bin/containerboot
--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -3,13 +3,13 @@
 # Make sure Tailscale IPv4 packets are forwarded to WireGuard
 iptables -C FORWARD -j ts-forward 2>/dev/null
 if [ $? -ne 0 ]; then
-  echo "The IPv4 firewall rules are not set up yet, failing healthcheck"
+  echo "The IPv4 firewall rules are not set up yet for, failing healthcheck"
   exit 1
 else
-  iptables -C FORWARD -s 100.64.0.0/10 ! -o "${WG_DEVICE}" -j DROP 2>/dev/null
-  if [ $? -eq 1 ]; then
-    echo "Enforcing IPv4 firewall rules for tailscale packets"
-    iptables -I FORWARD 1 -s 100.64.0.0/10 ! -o "${WG_DEVICE}" -j DROP
+  iptables -C FORWARD -j tg-forward 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Enforcing IPv4 firewall rules"
+    iptables -I FORWARD 1 -j tg-forward
   fi
 fi
 
@@ -19,9 +19,9 @@ if [ $? -ne 0 ]; then
   echo "The IPv6 firewall rules are not set up yet, failing healthcheck"
   exit 1
 else
-  ip6tables -C FORWARD -s fd7a:115c:a1e0::/48 ! -o "${WG_DEVICE}" -j DROP 2>/dev/null
-  if [ $? -eq 1 ]; then
-    echo "Enforcing IPv6 firewall rules for tailscale packets"
-    ip6tables -I FORWARD 1 -s fd7a:115c:a1e0::/48 ! -o "${WG_DEVICE}" -j DROP
+  ip6tables -C FORWARD -j tg-forward 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Enforcing IPv6 firewall rules"
+    ip6tables -I FORWARD 1 -j tg-forward
   fi
 fi
