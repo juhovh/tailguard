d2c9facc74877f3077c95fbd888504d49a93e4f4
Juho Vähä-Herttua <juhovh@iki.fi>
2025-11-01 09:56:58 +1100

    Make healthcheck more compact, improve logged messages

--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 update_firewall() {
-  $iptables -C $CHAIN -j "ts-$chain" 2>/dev/null
-  if [ $? -ne 0 ]; then
+  if ! $iptables -C $CHAIN -j "ts-$chain" 2>/dev/null; then
     echo "The $iptables ts-$chain rules are not set up yet, failing healthcheck"
     exit 1
   fi
-  $iptables -C $CHAIN -j "tg-$chain" 2>/dev/null
-  if [ $? -ne 0 ]; then
+  if ! $iptables -C $CHAIN -j "tg-$chain" 2>/dev/null; then
     echo "Enforcing $iptables tg-$chain rules"
     $iptables -I $CHAIN 1 -j "tg-$chain"
   fi
@@ -22,19 +20,15 @@ iptables="ip6tables -t nat" CHAIN="POSTROUTING" chain="postrouting" update_firew
 
 # Add PREROUTING rules for TS_DEST_IP addresses if needed
 for dest_ip in $(echo "${TS_DEST_IP}" | tr ',' '\n'); do
-  tailscale_ipv4=$(tailscale ip -4 2>/dev/null)
-  if [ -n "$tailscale_ipv4" ] && ipcalc -s -c -4 "$dest_ip"; then
-    iptables -t nat -C PREROUTING -d $tailscale_ipv4 -j DNAT --to-destination $dest_ip 2>/dev/null
-    if [ $? -ne 0 ]; then
-      echo "Routing IPv4 address $tailscale_ipv4 to $dest_ip"
+  if tailscale_ipv4="$(tailscale ip -4 2>/dev/null)" && ipcalc -s -c -4 "$dest_ip"; then
+    if ! iptables -t nat -C PREROUTING -d $tailscale_ipv4 -j DNAT --to-destination $dest_ip 2>/dev/null; then
+      echo "Forwarding Tailscale IPv4 address $tailscale_ipv4 to $dest_ip"
       iptables -t nat -A PREROUTING -d $tailscale_ipv4 -j DNAT --to-destination $dest_ip
     fi
   fi
-  tailscale_ipv6=$(tailscale ip -6 2>/dev/null)
-  if [ -n "$tailscale_ipv6" ] && ipcalc -s -c -6 "$dest_ip"; then
-    ip6tables -t nat -C PREROUTING -d $tailscale_ipv6 -j DNAT --to-destination $dest_ip 2>/dev/null
-    if [ $? -ne 0 ]; then
-      echo "Routing IPv6 address $tailscale_ipv6 to $dest_ip"
+  if tailscale_ipv6="$(tailscale ip -6 2>/dev/null)" && ipcalc -s -c -6 "$dest_ip"; then
+    if ! ip6tables -t nat -C PREROUTING -d $tailscale_ipv6 -j DNAT --to-destination $dest_ip 2>/dev/null; then
+      echo "Forwarding Tailscale IPv6 address $tailscale_ipv6 to $dest_ip"
       ip6tables -t nat -A PREROUTING -d $tailscale_ipv6 -j DNAT --to-destination $dest_ip
     fi
   fi
